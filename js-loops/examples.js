96d1d8e0fa6a176fbc5446d0c21ebb94148a955d
Charis Amend <charis.amend@googlemail.com>
2023-12-15 15:50:38 +0100

    updatesed sesion notees final with examples.js

--- js-loops/examples.js
+++ js-loops/examples.js
@@ -3,6 +3,7 @@ console.clear();
 const arr = [2, 4, 6, 8]
 let sum = 0;
 
+// how do we find out the sum of the array elements?
 // 1
 let length = arr.length
 let counter = 0
@@ -18,3 +19,11 @@ while (arr.length > 0) {
     // 20 = 20 + last deleted item in the array
 }
 console.log("result:", sum)
+
+
+// 2
+for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i]
+}
+console.log("result:", sum)
+
