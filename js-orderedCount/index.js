a32c3b73cb0ae6698b26273c4cffe9c5b945d4f0
Charis Amend <charis.amend@googlemail.com>
2024-01-08 10:53:36 +0100

    updated session notes for ordered count as well

--- js-orderedCount/index.js
+++ js-orderedCount/index.js
@@ -4,11 +4,19 @@ const orderedCount = function (text) {
     const abracadabraaText = "abracadabraa"
     const newArray = [...new Set(abracadabraaText)]
     console.log(newArray)
+    // logs this: [ 'a', 'b', 'r', 'c', 'd' ]
 
     // map over the new array and replace every character of the new Array with its own array
     return newArray.map(function (character) {
         return [character, text.split(character).length - 1]
     })
 }
+/** logs this: [
+  [ 'a', 6 ],
+  [ 'b', 2 ],
+  [ 'r', 2 ],
+  [ 'c', 1 ],
+  [ 'd', 1 ]
+] **/
 
 orderedCount("abracadabraa")
