ceeb4caade5de46d0ae5a0e85f7cdff3c9f6b2d1
Charis Amend <charis.amend@googlemail.com>
2024-01-15 15:12:08 +0100

    tryied dcommenting

---
+++ react-props/demo-start/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Pet from "./Pet.js";
+import "./styles.css";
+
+export default function App() {
+  function handleAnimal(animal) {
+    console.log(`Hello ${name}, said ${sound}`)
+  }
+
+  return (
+    // adding the Pet() function fom Pet.js:
+    <div>
+      {/* adding the function for each animal and getting different keys from each animal: */}
+      <Pet sound="Meow" name="Cat" onhandleAnimal={handleAnimal} isHungry />
+      <Pet sound="Woof" name="Dog" onhandleAnimal={handleAnimal} />
+      <Pet sound="Squeak" name="Mouse" onhandleAnimal={handleAnimal} isHungry />
+
+    </div>
+
+  );
+}
--- react-props/demo-start/src/Pet.js
+++ react-props/demo-start/src/Pet.js
@@ -1,24 +1,23 @@
 export default function Pet({ name, sound, emoji, onhandleAnimal, isHungry }) {
-    console.log("props:", props)
+    // console.log("props:", props)
 
-
-    function feedMe() {
-        if (isHungry) {
-            return "Feed Me!"
-        } else {
-            return sound
-        }
-    }
+    // function feedMe() {
+    //     if (isHungry) {
+    //         return "Feed Me!"
+    //     } else {
+    //         return sound
+    //     }
+    // }
 
     // we need the function to run only when we click! 
     // so we put it inside a callback function
     // onClick={() => function (parameters)}
     return (< div onClick={() => onhandleAnimal(name, sound)} className="animal" >
 
-    // if condition for if animal has isHungry, then text Feed me shows
-    // {isHungry ? "Feed me!" : sound}
-    // or calling function feedMe above
-        {feedMe()}
+        {/*if condition for if animal has isHungry, then text Feed me shows*/}
+        {isHungry ? "Feed me!" : sound}
+        {/*or calling function feedMe above */}
+        {/* {feedMe()} */}
         < span role="img" aria-label={name}>
             {emoji}
         </span >
