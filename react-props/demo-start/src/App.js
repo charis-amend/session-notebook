c17c24367ed9293c41ddc845e7bf5535afb8655b
Charis Amend <charis.amend@googlemail.com>
2024-01-15 14:53:26 +0100

    updatees notes and demo

--- react-props/demo-start/src/App.js
+++ react-props/demo-start/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     // adding the Pet() function fom Pet.js:
     <div>
-    // adding the function for each animal and getting different keys from each animal:
+      {/* adding the function for each animal and getting different keys from each animal: */}
       <Pet sound="Meow" name="Cat" onhandleAnimal={handleAnimal} isHungry />
       <Pet sound="Woof" name="Dog" onhandleAnimal={handleAnimal} />
       <Pet sound="Squeak" name="Mouse" onhandleAnimal={handleAnimal} isHungry />
--- react-props/demo-start/src/Pet.js
+++ react-props/demo-start/src/Pet.js
@@ -1,6 +1,7 @@
 export default function Pet({ name, sound, emoji, onhandleAnimal, isHungry }) {
     console.log("props:", props)
 
+
     function feedMe() {
         if (isHungry) {
             return "Feed Me!"
@@ -9,20 +10,19 @@ export default function Pet({ name, sound, emoji, onhandleAnimal, isHungry }) {
         }
     }
 
-    return (
-        // we need the function to run only when we click! 
-        // so we put it inside a callback function
-        // onClick={() => function (parameters)}
-        < div onClick={() => onhandleAnimal(name, sound)
-        } className="animal" >
+    // we need the function to run only when we click! 
+    // so we put it inside a callback function
+    // onClick={() => function (parameters)}
+    return (< div onClick={() => onhandleAnimal(name, sound)} className="animal" >
+
     // if condition for if animal has isHungry, then text Feed me shows
     // {isHungry ? "Feed me!" : sound}
-    // or:
-            {feedMe()}
-            < span role="img" aria - label={name}>
+    // or calling function feedMe above
+        {feedMe()}
+        < span role="img" aria-label={name}>
             {emoji}
         </span >
-        </div >
+    </div >
 
     )
 }
