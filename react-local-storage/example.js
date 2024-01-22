2079b9c6ff14d397849692a1faed221a045b531b
Charis Amend <charis.amend@googlemail.com>
2024-01-22 10:55:09 +0100

    update

--- react-local-storage/example.js
+++ react-local-storage/example.js
@@ -1,6 +1,59 @@
+// ------------- VANILAA JS EXAMPLE ----------------
 const button = document.querySelector("#cookiebanner button")
 const cookieBanner = document.querySelector("#cookiebanner")
-button.addEventListener("click", () => {
-    cookieBanner.style.display = "none"
-})
+const consent = localStorage.getItem("consent")
 
+try {
+    if (consent === "ok") {
+        cookieBanner.style.display = "none";
+    }
+
+    button.addEventListener("click", () => {
+        cookieBanner.style.display = "none";
+        localStorage.setItem("consent", "ok")
+    })
+
+} catch (error) {
+    console.log(error)
+}
+// saaving object in localStorage:
+localStorage.setItem("person", JSON.stringify({ name: "klaus", age: 34 }))
+// logs the object as a string:
+'{ "name": "klaus", "age":34 }'
+// change the stringified object into a object again:
+const javaScriptObject = localStorage.getItem("person")
+JSON.parse(javaScriptObject)
+// returns: 
+{ name: "klaus"; age: 34 }
+
+// ------------- REACT EXAMPLE ----------------
+// adding this, so then, even if we open/close the browser tab the newly created todos item 
+// will still show up.
+import useLocalStorageState from "use-local-storage-state"
+export default function App() {
+    const [todos, setTodos] = useLocalStorageState("todos", {
+        defaultValue: [],
+    })
+}
+// or with a default value:
+import useLocalStorageState from "use-local-storage-state"
+export default function App() {
+    const [todos, setTodos] = useLocalStorageState("todos", {
+        defaultValue: [
+            {
+                id: 9999,
+                title: "default title",
+                isChecked: true
+            }
+        ],
+    })
+}
+
+function handleAddTodo(title) {
+    setTodos([{
+        id: uid(),
+        title,
+        isChecked: false,
+    }, todos,
+    ])
+}
--- react-local-storage/morning-notes.md
+++ react-local-storage/morning-notes.md
@@ -9,6 +9,7 @@ if we refresh browsers, the previous made changes on the website should be saved
 
 ## session storage and local storage
 
+- in dev console tools viewable
 - client-side
 - never save sensitive data!
 - this is only client-side, but still if you save other people's user data then only stuff like dark mode
@@ -24,9 +25,34 @@ if we refresh browsers, the previous made changes on the website should be saved
 ## local storage
 
 - available until the browser cache is being deleted
+- always saves any item in local storage as a string
+  - `localStorage.setItem("keyvalue", 3)`
+  - --> would return "string" even though 3 is a number
+  - "string-ifies" a number and boolean etc.!
+  - but not possible for an object!!
 
---------- EXAMPLE -----------
+## VANILLA JS:
 
-```js
+## saving cookiebanner selection in local storage:
 
-```
+---> see example.js
+
+1.  Eventlistener for button
+2.  localStorage. getItem, setItem and deleteItem possible
+3.  if condition if local storage item is status "ok"
+4.  add try-catch in case seomthing happens
+
+## Stringifying
+
+- saving object in localStoraage with localStorage.setItem("")
+- but saves object as a string
+- change it back to an object:
+  `const javaScriptObject = localStorage.getItem("person")`
+  `JSON.parse(javaScriptObject)`
+
+# Local Storage with REACT
+
+- react has a library for this local storage
+- using `useState()` to set new `localStorage.set` state:
+- import
+  `import useLocalStorageState from "use-local-storage-state"`
