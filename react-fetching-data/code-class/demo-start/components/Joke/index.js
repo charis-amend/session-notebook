a9f822bb228090e84851e58b938e95d9aec7a397
Charis Amend <charis.amend@googlemail.com>
2024-01-30 10:41:59 +0100

    fupdate

--- react-fetching-data/code-class/demo-start/components/Joke/index.js
+++ react-fetching-data/code-class/demo-start/components/Joke/index.js
@@ -1,26 +1,38 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-function useFetch(url) {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    async function startFetching() {
-      const response = await fetch(url);
-      const newData = await response.json();
-
-      setData(newData);
-    }
-
-    startFetching();
-  }, [url]);
-
-  return data;
-}
+// in _app.js file: 
+// const fetcher = (url) => fetch(url).then((response) => response.json())
 
 export default function Joke() {
   const [id, setId] = useState(0);
+  // adding the feature if the joke isFunny or not:
+  const [jokesInfo, setJokesInfo] = useState([]);
 
-  const data = useFetch(`https://example-apis.vercel.app/api/bad-jokes/${id}`);
+  const { data, error, isValidating, isLoading } = useSWR(`https://example-apis.vercel.app/api/bad-jokes/${id}`);
+  console.log("data", data)
+
+
+  function handleToggleJoke(id) {
+    console.log("id", id)
+
+    // set the variable for the info: 
+    const info = jokesInfo.find((info) => info.id === id)
+    // isFunny default is false
+
+    if (info) {
+      const newInfo = jokesInfo.map((info) => info.id == id ? { ...info, isFunny: !info.isFunny } : info)
+      // or { id: info.id , isFunny: !info.isFunny }
+
+      // set the new Joke INfo: 
+      setJokesInfo(newInfo)
+    } else {
+      const newInfo = [...jokesInfo, { id: id, isFunny: true }]
+      setJokesInfo(newInfo)
+    }
+  }
+  const info = jokesInfo?.find((info) => info.id === id) ?? { isFunny: false }
+  const { isFunny } = info;
 
   function handlePrevJoke() {
     setId(data.prevId);
@@ -37,7 +49,16 @@ export default function Joke() {
   return (
     <>
       <small>ID: {id}</small>
-      <h1>{data.joke}</h1>
+      <h1>{data.joke}
+        <span>
+          {info.isFunny ? "🤣" : "🥲"}
+        </span>
+      </h1>
+
+      <button type="button" onClick={() => console.log("toggle Button isFunny")}>
+        {info.isFunny ? "start laughing" : "stop laughing"}
+      </button>
+
       <div>
         <button type="button" onClick={handlePrevJoke}>
           ← Prev Joke
--- react-fetching-data/code-class/demo-start/pages/_app.js
+++ react-fetching-data/code-class/demo-start/pages/_app.js
@@ -1,10 +1,15 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+const { data, error, isValidating, isLoading } = useSWR(`https://example-apis.vercel.app/api/bad-jokes/${id}`, fetcher);
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
